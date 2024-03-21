cecff4df298cf0ea213dd50451231cf6a2b6b71b
박승찬 <seungchan141414@gmail.com>
2024-03-21 22:53:47 +0900

    [Feat] H2 데이터 베이스 등록

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 1', '내용 1', NOW(), NOW())
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 2', '내용 2', NOW(), NOW())
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 3', '내용 3', NOW(), NOW())
+INSERT INTO users (email, password)
+VALUES ('123@123', '$2a$10$8uBHT/TlmSFNbaUeqZKeceOC2GHHj92oeTEMJLTA0x/NNkYwIqZ2S');
+
+INSERT INTO article (title, content, created_at, updated_at)
+VALUES ('제목 1', '내용 1', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at)
+VALUES ('제목 2', '내용 2', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at)
+VALUES ('제목 3', '내용 3', NOW(), NOW());
