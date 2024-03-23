006e17e9db7f5b5c781f734d786445246fd1b20b
박승찬 <seungchan141414@gmail.com>
2024-03-23 09:36:02 +0900

    [Style] 주석 추가

--- src/main/java/com/example/spring_mvc/service/UserDetailService.java
+++ src/main/java/com/example/spring_mvc/service/UserDetailService.java
@@ -14,17 +14,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailService implements UserDetailsService { // 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    /**
-     * 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드 s ssss
-     *
-     * @param email the username identifying the user whose data is required.
-     * @return
-     */
-    @Override
-    public User loadUserByUsername(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException((email)));
-    }
+  /**
+   * 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드 s
+   *
+   * @param email the username identifying the user whose data is required.
+   * @return
+   */
+  @Override
+  public User loadUserByUsername(String email) {
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new IllegalArgumentException((email)));
+  }
 }
